fb2605ea3b2afab37b054ca4b567396d86bb5682
Dblackone <61014349+Dblackone@users.noreply.github.com>
2022-08-03 10:51:31 +0100

    added madlibs

---
+++ MADLIBS/madlibs.py
@@ -0,0 +1,11 @@
+
+
+madlib_type = input("what game do you want to play? ")
+adj = input("Enter an adjective: ")
+adj2 = input("Enter another adjective: ")
+adj3 = input("Enter another adjective: ")
+
+
+madlib = f"Congratulations! You have successfully completed the {madlib_type} Madlib. The story is as follows: Computer programing is {adj}, but it's not as fun as {madlib_type}. It sometimes makes me {adj2} and some times it makes me so {adj3}. "
+
+print(madlib)
