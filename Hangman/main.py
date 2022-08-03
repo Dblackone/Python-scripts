d9c087634ba8555405e43b39c81b65813e463129
Dblackone <61014349+Dblackone@users.noreply.github.com>
2022-08-03 16:52:11 +0100

    Uodate to Hangman

--- Hangman/main.py
+++ Hangman/main.py
@@ -1,2 +1,21 @@
 
+import random
+import string
 
+from words import words
+
+print(words)
+
+def get_valid_word(words):
+    word = random.choice(words)
+    while '-' in word or '' in word
+        word = random.choice(words)
+    return word
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_letter = set()
+
+    used_letter =
