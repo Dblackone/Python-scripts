ef77465ba3092196a3311c404cc5dd3c292ea796
Dblackone <61014349+Dblackone@users.noreply.github.com>
2022-08-04 10:46:53 +0100

    update hangman

--- Hangman/main.py
+++ Hangman/main.py
@@ -1,21 +1,49 @@
-
 import random
 import string
-
+from hangma_visual import lives_visual_dict
 from words import words
 
-print(words)
 
 def get_valid_word(words):
     word = random.choice(words)
-    while '-' in word or '' in word
+    while '-' in word or '' in word:
         word = random.choice(words)
-    return word
+
+    return word.upper()
+
 
 def hangman():
     word = get_valid_word(words)
     word_letters = set(word)
     alphabet = set(string.ascii_uppercase)
-    used_letter = set()
+    used_letters = set()
 
-    used_letter =
+    lives = 7
+
+    while len(word_letters) > 0 and lives >0:
+        print('You have', lives, 'lives left and your have used this letters: ', ''.join(used_letters))
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print(lives_visual_dict[lives])
+        print('current word: ', ' '.join(word_list))
+
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+                print('')
+            else:
+                lives = lives - 1
+                print('/nYou have')
+
+        elif user_letter in used_letters:
+            print('you already used this letter try again')
+
+        else:
+            print('invalid')
+
+
+hangman()
+
+
+
